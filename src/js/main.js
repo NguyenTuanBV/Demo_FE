ba589750f2db5740fae930d15b36179e7a82be83
tuan <tuantongthai@gmail.com>
2025-03-05 08:22:59 +0700

    update

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+
+// $(document).ready(function(){
+//   $('.review-slide').owlCarousel({
+//     loop: false,
+//     margin: 20,
+//     dots: false,
+//     nav: true,
+//     navText: ["<img src='../src/image/arrow-left.png'/>","<img src='../src/image/arrow-right.png'/>"],
+//     rewind: true,
+//     // autoplay: true,
+//     // autoplayTimeout: 4000,
+//     smartSpeed: 1000,
+//     responsive:{
+//         0:{
+//             items: 1
+//         },
+//         820:{
+//             items: 1
+//         },
+//         1300:{
+//             items: 1
+//         }
+//     }
+//   })
+// })
