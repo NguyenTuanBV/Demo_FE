57af773b8c400912a719f463ec466d070bd05a24
tuan <tuantongthai@gmail.com>
2025-03-06 17:10:57 +0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,32 @@
+$(document).ready(function(){
+  $('.category-slide').owlCarousel({
+    loop: false,
+    margin: 14,
+    dots: false,
+    nav: true,
+    navText: ["<img src='../src/image/arrow-left.png'/>","<img src='../src/image/arrow-right.png'/>"],
+    rewind: true,
+    // autoplay: true,
+    // autoplayTimeout: 4000,
+    smartSpeed: 1000,
+    responsive:{
+        0:{
+            items: 2
+        },
+        540:{
+            items: 2
+        },
+        820:{
+            items: 3
+        },
+        1440:{
+            items: 4
+        },
+        1920:{
+            items: 6
+        }
+    }
+  })
+})
 
-// $(document).ready(function(){
-//   $('.review-slide').owlCarousel({
-//     loop: false,
-//     margin: 20,
-//     dots: false,
-//     nav: true,
-//     navText: ["<img src='../src/image/arrow-left.png'/>","<img src='../src/image/arrow-right.png'/>"],
-//     rewind: true,
-//     // autoplay: true,
-//     // autoplayTimeout: 4000,
-//     smartSpeed: 1000,
-//     responsive:{
-//         0:{
-//             items: 1
-//         },
-//         820:{
-//             items: 1
-//         },
-//         1300:{
-//             items: 1
-//         }
-//     }
-//   })
-// })
+
